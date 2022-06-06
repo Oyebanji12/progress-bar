e34f1e44406c784ac0dc3d5f591f12c770da187e
oyebanji12 <oyebanjiqudus81@gmail.com>
2022-06-06 07:27:52 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,53 @@
+
+var progress= document.getElementById('progress');
+var prev= document.getElementById('prev');
+var next= document.getElementById('next');
+var circles= document.querySelectorAll('.circle')
+
+
+let currentActive=1;
+
+next.addEventListener('click', function () {
+    currentActive++;
+    if(currentActive > circles.length){
+        currentActive = circles.length;
+    }
+ 
+    update()
+})
+
+prev.addEventListener('click', function () {
+    currentActive--;
+    if(currentActive < 1){
+        currentActive=1
+    }
+
+   update(); 
+})
+
+function update() {
+    circles.forEach(function (circle, idx) {
+        if(idx < currentActive){
+            circle.classList.add('active')
+        } else{
+            circle.classList.remove('active')
+        }
+        
+    })
+
+
+    var actives=document.querySelectorAll('.active');
+    progress.style.width=(actives.length - 1) / (circles.length - 1) * 100 + '%';
+    if(currentActive === 1){
+        prev.disable = true
+    } else if(currentActive === circles.length){
+        next.disable=true
+    } else {
+        prev.disable=false;
+        next.disable=false;
+    }
+    
+}
+
+
+
